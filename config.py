541b69725f03e11766821c1a0403e5ef52eccfc0
Tim <hello@timbest.net>
2017-12-27 00:27:44 -0800

    trial landing page

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-ROOT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-OUTPUT_DIRECTORY = os.path.join(ROOT_DIRECTORY, '_site/')
-SITE_DIRECTORY = os.path.join(ROOT_DIRECTORY, 'site/')
-
-# Relative to SITE_DIRECTORY
-STATIC_PATH = 'static/'
