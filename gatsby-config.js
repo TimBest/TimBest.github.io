20cb67e33bfdc4bafacfd7e792c649a86557642b
Tim <hello@timbest.net>
2019-01-18 23:03:04 -0800

    use the same name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Tim Best`,
-    description: `Tim Best's website`,
-    author: `Tim Best`,
+    title: `Timonthy Best`,
+    description: `Timothy Best's website`,
+    author: `Timothy Best`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -22,8 +22,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#852237`,
+        theme_color: `#852237`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
