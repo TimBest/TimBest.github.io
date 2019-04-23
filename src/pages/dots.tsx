efdbcb0999681472a8819ede7de7baf74f94418b
Tim <hello@timbest.net>
2019-04-23 15:23:45 -0700

    fix jittery updating

--- src/pages/dots.tsx
+++ src/pages/dots.tsx
@@ -2,17 +2,11 @@ import React from 'react'
 import SEO from '../components/seo'
 import '../images/dots.css'
 
-/*
-// TODO:
- - clean up triangle removeal logic
- - only regenerate when needed
-*/
-
 function getRandomInt(max: number): number {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function getRandomArbitrary(min: number, max: number): number {
+function getRandomFloat(min: number, max: number): number {
   return Math.random() * (max - min) + min;
 }
 
@@ -40,10 +34,10 @@ interface DotColorParams {
 
 class Dots extends React.Component<DotsProps, DotsState> {
   rows = 4
+  minColumns = 1
   xPadding = 35
   yPadding = 3
-  minError = -3
-  maxError = 3
+  offset = 6
   dotDiameter = 100
   dotRadius = this.dotDiameter / 2
 
@@ -70,27 +64,34 @@ class Dots extends React.Component<DotsProps, DotsState> {
     })
   }
 
-  getWidth() {
-    return window.innerWidth || document.body.clientWidth
+  componentWillUnmount(): void {
+    window.removeEventListener("resize", this.updateDimensions.bind(this))
   }
 
-  getRandPadding() {
-    return getRandomArbitrary(this.minError, this.maxError)
+  getWidth(): number {
+    return window.innerWidth || document.body.clientWidth
   }
 
   /**
    * Calculate & Update state of new dimensions
    */
   updateDimensions() {
-    // TODO: this only needs to update state when width or height changes more then the shape size
-    this.setState({
-      width: this.getWidth(),
-      dots: this.generateDots()
-    })
-  }
+    const previousColumns = this.numberOfColumns(this.state.width);
+    const width = this.getWidth();
+    const columns = this.numberOfColumns(this.getWidth());
+    console.log(previousColumns, columns)
+    console.log(width, this.getWidth())
 
-  getRandomColor() {
-    return this.props.colors[getRandomInt(this.props.colors.length)]
+    if (previousColumns === columns || columns <= this.minColumns) {
+      this.setState({width});
+      return;
+    }
+
+    const dots = (previousColumns < columns) ?
+                 this.appendColumn(Array.from(this.state.dots)) :
+                 this.removeColumn(Array.from(this.state.dots));
+
+    this.setState({width, dots})
   }
 
   isEqual(x: string, y: string, z: string): boolean {
@@ -120,59 +121,102 @@ class Dots extends React.Component<DotsProps, DotsState> {
     return true;
   }
 
+  getRandomColor(): string {
+    return this.props.colors[getRandomInt(this.props.colors.length)];
+  }
+
+  getColor(dots?: DotColorParams): string {
+    let color = this.getRandomColor();
+    while (!this.isVaildColor(color, dots)) {
+      color = this.getRandomColor();
+    }
+    return color;
+  }
+
+  getOffset() {
+    return getRandomFloat(0, this.offset)
+  }
+
+  getY(row: number): number {
+    const y = this.dotRadius + (this.dotDiameter * row);
+    const padding = this.yPadding * row;
+    return y + padding + this.getOffset();
+  }
+
+  getX(row: number, column: number): number {
+    const x = this.dotRadius + (this.dotDiameter * column);
+    const padding = this.xPadding * column;
+    const indent = row % 2 ? this.dotRadius + (this.xPadding/2) : 0;
+    return x + padding + indent + this.getOffset();
+  }
+
+  removeColumn(dots: Dot[][]): Dot[][] {
+    dots.forEach(row => row.pop());
+    return dots
+  }
+
+  appendColumn(dots: Dot[][]): Dot[][] {
+    dots.forEach((row, index) => {
+      const length = row.length
+      const y = this.getY(index);
+      const x = this.getX(index, length);
+      let dotColorParams: DotColorParams | undefined;
+      if (index > 0) {
+        dotColorParams = {
+          left: row[index-1].color,
+          top: dots[index-1][length-1].color,
+          topLeft: dots[index-1][length-2].color,
+        };
+      }
+      const color = this.getColor(dotColorParams);
+      row.push({x, y, color});
+    });
+    return dots
+  }
+
+  numberOfColumns(width: number): number {
+    return Math.floor(width / (this.offset + this.dotDiameter + this.xPadding))
+  }
+
   generateDots() {
     const dots: Dot[][] = [];
-    const columns = (this.getWidth() / (this.maxError + this.dotDiameter + this.xPadding)) - 1
-    var height: number = (this.dotRadius) + Math.abs(this.minError);
+    const columns = this.numberOfColumns(this.getWidth());
 
     for(var i = 0; i < this.rows; i++) {
-      var width: number = this.dotRadius;
-      if (i % 2 === 0) {
-        // indent even numbered rows by half a dot to create stagared rows
-        width = width + this.dotRadius + (this.xPadding/2)
-      }
+      const y = this.getY(i);
+
       const row: Dot[] = [];
-      for(var j = 0; j <= columns; j++) {
-        var postionX = width + this.getRandPadding();
-        var postionY = height + this.getRandPadding();
+      for(var j = 0; j < columns; j++) {
+        const x = this.getX(i, j);
 
         let dotColorParams: DotColorParams | undefined;
         if (i > 0 && j > 0) {
-
           dotColorParams = {
             left: row[j-1].color,
             top: dots[i-1][j].color,
             topLeft: dots[i-1][j-1].color,
-            topRight: (j < columns - 1) ? dots[i-1][j+1].color : undefined,
+            topRight: (j < columns - 2) ? dots[i-1][j+1].color : undefined,
           };
         }
-        let color = this.getRandomColor();
-        while (!this.isVaildColor(color, dotColorParams)) {
-          color = this.getRandomColor();
-        }
-
-        row.push({
-          color,
-          x: postionX,
-          y: postionY
-        })
-        width += this.dotDiameter + this.xPadding
+        const color = this.getColor(dotColorParams);
+        row.push({x, y, color});
       }
       dots.push(row);
-      height += this.dotDiameter + this.yPadding
     }
     return dots
   }
 
   render() {
-    const height = ((this.dotDiameter + this.yPadding) * this.rows) + this.maxError
+    const height = ((this.dotDiameter + this.yPadding) * this.rows) + this.offset;
+    const columns = this.state.dots[0].length;
+    const width = ((this.dotDiameter + this.xPadding) * columns) + this.offset + this.dotRadius;
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
         version="1.1"
-        viewBox={`0 0 ${this.state.width} ${height}`}
+        viewBox={`0 0 ${width} ${height}`}
         height={height}
-        width={this.state.width}
+        width={width}
         style={{marginBottom: "100px"}}
       >
         <filter id="filter">
@@ -200,16 +244,15 @@ class Dots extends React.Component<DotsProps, DotsState> {
             result="composite"
           />
         </filter>
-        {this.state.dots.map(row => (
+        {this.state.dots.map((row, index) => (
             row.map(dot => (
-              <>
+              <g key={`${dot.x},${dot.y},${index}`}>
                 <circle
                   cx={dot.x}
                   cy={dot.y}
                   r={this.dotRadius}
                   fill={dot.color}
                   filter="url(#filter)"
-                  key={`${dot.x},${dot.y}`}
                 />
                 <circle
                   cx={dot.x}
@@ -217,9 +260,8 @@ class Dots extends React.Component<DotsProps, DotsState> {
                   r={this.dotRadius}
                   fill={dot.color}
                   filter="url(#filter)"
-                  key={`${dot.x},${dot.y}A`}
                 />
-              </>
+              </g>
             ))
         ))}
        </svg>
@@ -230,10 +272,12 @@ class Dots extends React.Component<DotsProps, DotsState> {
 const DotsPage = () =>  (
   <div className="dots">
     <SEO title="dots"/>
-    <Dots colors={['#203593', '#b0437c', '#d6ae37']}/>
-    <Dots colors={['#556f3f', '#c25247', '#d3841f']}/>
-    <Dots colors={['#d6ae37', '#556f3f', '#6f238b']}/>
-    <Dots colors={['#b0437c' , '#6b502c' , '#d3841f']}/>
+    <div style={{display: "flex", justifyContent: "center", flexWrap: "wrap"}}>
+      <Dots colors={['#203593', '#b0437c', '#d6ae37']}/>
+      <Dots colors={['#556f3f', '#c25247', '#d3841f']}/>
+      <Dots colors={['#d6ae37', '#556f3f', '#6f238b']}/>
+      <Dots colors={['#b0437c' , '#6b502c' , '#d3841f']}/>
+    </div>
   </div>
 )
 
